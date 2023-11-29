18f5623a7bc9778781aabea512c70e5b75819de2
Kropa <k0n42525@gmail.com>
2023-11-29 11:58:13 +0100

    Created class Calculator with methods without mathematical logic

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+
+        public double add( )
+        {
+            return 0;
+        }
+
+        public double subtract() 
+        { 
+            return 0;
+        }
+        
+        public double multiply()
+        {
+            return 0;
+        }
+
+        public double divide()
+        {
+            return 0;
+        }
+
+        public double exponation() 
+        {
+            return 0;
+        }
+
+        public double factorial() 
+        { 
+            return 0;
+        }
+    }
+}
